cea157c5421153432ca7bad134b1cf84f701d692
Jindrich Vimr <jindrich@vimr.cz>
2012-11-19 22:05:35 +0000

    mysql db install

--- manifests/gs.pp
+++ manifests/gs.pp
@@ -1,1 +1,16 @@
-include 'puppetlabs/mysql'
+
+class { 'mysql': }
+
+class { 'mysql::server' :
+  config_hash => { 'root_password' => 'alabama' }
+}
+
+mysql::db { 'globalsight':
+  charset => 'utf8',
+  user => 'globalsight',
+  password => 'globalsight',
+  host => 'localhost',
+  grant => ['all'],
+}
+
+
