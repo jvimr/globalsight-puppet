8609ce659b73094eacd0d8f7171cc7709da4c738
Jindrich Vimr <jindrich@vimr.cz>
2012-11-20 09:11:49 +0000

    corrected custom mysql config file name

--- manifests/gs.pp
+++ manifests/gs.pp
@@ -10,7 +10,7 @@ class { 'mysql::server' :
 }
 
 
-#mysql::server::config { 'basic_config':
+#mysql::config { 'basic_config':
 #   settings=> "[mysqld]\nlower_case_table_names=1",
 #}
 
@@ -23,7 +23,7 @@ mysql::db { 'globalsight':
   require => Class['mysql::server']  ,
 }
 
-file { '/etc/mysql/conf.d/gs-mysqld-lowercase-table-names.my.cnf':
+file { '/etc/mysql/conf.d/gs-mysqld-lowercase-table-names_my.cnf':
    require => Package['mysql-server'],
    content => "[mysqld]\nlower_case_table_names=1",
 }
