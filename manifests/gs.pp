33e7253e4e0480a40a89bd8e3ca6db1deb1adefa
Jindrich Vimr <jindrich@vimr.cz>
2012-11-20 09:54:01 +0000

    more packages for globalsight

--- manifests/gs.pp
+++ manifests/gs.pp
@@ -5,7 +5,8 @@
 class { 'mysql': }
 
 class { 'mysql::server' :
-  config_hash => { 'root_password' => 'alabama' }
+  config_hash => { 'root_password' => 'alabama' },
+  #subscribe => File['/etc/mysql/conf.d/gs-mysqld-lowercase-table-names_my.cnf'],
 
 }
 
@@ -20,10 +21,67 @@ mysql::db { 'globalsight':
   password => 'globalsight',
   host => 'localhost',
   grant => ['all'],
-  require => Class['mysql::server']  ,
+#  require => Class['mysql::server']  ,
+  require => File['/root/.my.cnf']
+
 }
 
 file { '/etc/mysql/conf.d/gs-mysqld-lowercase-table-names_my.cnf':
-   require => Package['mysql-server'],
    content => "[mysqld]\nlower_case_table_names=1",
+   before=> Exec['mysqld-restart']
+}
+
+
+#package & service postfix
+package { 'postfix':
+      ensure => present,
+      before => Service['postfix'],
+    }
+
+    service { 'postfix':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => true,
+#      subscribe  => File['/etc/ssh/sshd_config'],
+    }
+#user jboss
+user { 'jboss':
+   ensure=> 'present',
+   home=>'/home/jboss'
+}
+
+
+package { 'openjdk-7-jdk':
+  ensure=> present,
+
+}
+
+package { 'libdb-dev':
+  ensure => present,
+
+}
+
+package { 'make':
+ ensure => present,
+
+}
+
+package { 'gcc':
+  ensure => present,
+
+}
+
+
+package { 'autoconf':
+  ensure => present,
+
+}
+
+package { 'wget' :
+ ensure => present,
+}
+
+package { 'nginx' :
+ ensure => present,
 }
