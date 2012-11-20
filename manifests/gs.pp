b7668a79d2094ed06eccca7c62fa18771a8aa252
Jindrich Vimr <jindrich@vimr.cz>
2012-11-20 10:50:43 +0000

    openldap make install steps

--- manifests/gs.pp
+++ manifests/gs.pp
@@ -69,7 +69,7 @@ package { 'make':
 
 package { 'gcc':
   ensure => present,
-
+ before => Exec["./configure-ldap"],
 }
 
 
@@ -78,10 +78,56 @@ package { 'autoconf':
 
 }
 
-package { 'wget' :
- ensure => present,
-}
 
 package { 'nginx' :
  ensure => present,
 }
+
+package { 'wget':
+ ensure => present,
+}
+
+exec { "getopenldap":
+      command => "wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.33.tgz",
+      cwd => "/tmp/",
+	path=> '/usr/bin'
+}
+
+exec { "untaropenldap":
+       command => '/bin/tar -xzf openldap-2.4.33.tgz',
+       cwd => "/tmp/",
+       require => Exec["getopenldap"],
+	creates => '/tmp/openldap-2.4.33/',
+ }
+
+exec { "./configure-ldap":
+      command => '/tmp/openldap-2.4.33/configure --prefix=/usr/local/openldap',
+      cwd => "/tmp/openldap-2.4.33/",
+	creates => '/tmp/openldap-2.4.33/Makefile',      
+      require => Exec["untaropenldap"]
+}
+
+exec { 'make_dep_openldap':
+  command => 'make depend',
+  cwd => "/tmp/openldap-2.4.33/",
+  require => Exec['./configure-ldap'],
+  path => '/bin:/usr/bin:/usr/local/bin'
+}
+
+
+exec { 'make_openldap':
+  command => 'make -j2',
+  timeout => '1000',
+  cwd => "/tmp/openldap-2.4.33/",
+  require => Exec['make_dep_openldap'],
+  path => '/bin:/usr/bin:/usr/local/bin'
+}
+
+
+exec { 'make_install_openldap':
+  command => 'make install',
+  cwd => "/tmp/openldap-2.4.33/",
+  require => Exec['make_openldap'],
+  path => '/bin:/usr/bin:/usr/local/bin'
+}
+
